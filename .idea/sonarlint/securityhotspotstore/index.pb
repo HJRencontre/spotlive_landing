a3044e3c65650d242a83fa76503bfa4ecf00023d
Hermann-Jude Rencontre <h.jrencontre@outlook.com>
2024-06-13 21:30:08 +0200

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,26 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+>
+pages/_app.tsx,5/5/550528cc072fa7d76d123e2f4303d4ab39daa8e8
+E
+components/navbar.tsx,7/a/7a89fc1e301156e0cce3c97857ca7b934f6c4ac6
+B
+styles/globals.css,8/c/8c258232cbcd5d83991c524a9e86c8d3f984fc57
+E
+pages/about/index.tsx,c/2/c2a189f3cff18ca76654cc37ca566de5561662b7
+C
+pages/_document.tsx,a/d/ada43e48670fd1ae7318859ac38479e031ea9236
+@
+layouts/head.tsx,4/f/4fb28b3f6d3d9977d53259e4031108456a58c415
+C
+layouts/default.tsx,8/3/83b5a03bcd0d2280403e239ffda23595d5566cbb
+>
+.eslintrc.json,7/1/71c20a341bfe32aa5cd5b4384e94f942bab6a7d3
+D
+components/icons.tsx,2/e/2e00cd9043e27bf1a3706dcc48a1f9ad73576333
+B
+tailwind.config.js,0/9/0918cbb76f2c44f223706cec9d31a32cd1177ce4
---
+++ components/navbar.tsx
@@ -0,0 +1,87 @@
+import {
+  Button,
+  Kbd,
+  Link,
+  Input,
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarBrand,
+  NavbarItem,
+  NavbarMenuItem,
+  link as linkStyles,
+} from "@nextui-org/react";
+import NextLink from "next/link";
+import clsx from "clsx";
+
+import { siteConfig } from "@/config/site";
+import { ThemeSwitch } from "@/components/theme-switch";
+import {
+  TwitterIcon,
+  GithubIcon,
+  DiscordIcon,
+  HeartFilledIcon,
+  SearchIcon,
+} from "@/components/icons";
+import { Logo } from "@/components/icons";
+
+export const Navbar = () => {
+  const searchInput = (
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: "bg-default-100",
+        input: "text-sm",
+      }}
+      endContent={
+        <Kbd className="hidden lg:inline-block" keys={["command"]}>
+          K
+        </Kbd>
+      }
+      labelPlacement="outside"
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+      }
+      type="search"
+    />
+  );
+
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <img
+              src="https://raw.githubusercontent.com/HJRencontre/MDS-2023-Lean-Startup/7bfe9ddd26904686f8d23103dcc191ac1ab12684/src/assets/images/typography.png"
+              className="w-24 md:w-40"  alt={"Spotlive logo"}/>
+        </NextLink>
+      </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="hidden sm:flex gap-2">
+          <Button
+            radius="full"
+            className="border-2 btn btn-outline border-lime bg-blueMidnight text-lime">
+            <Link isExternal href={siteConfig.links.twitter}>
+              Je suis organisateur
+            </Link>
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <Link isExternal href={siteConfig.links.github}>
+          <GithubIcon className="text-default-500" />
+        </Link>
+        <ThemeSwitch />
+        <NavbarMenuToggle />
+      </NavbarContent>
+    </NextUINavbar>
+  );
+};
